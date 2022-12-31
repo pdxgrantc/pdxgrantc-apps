16743789ff5a4eabdd8637d76c77342a7b72081e
pdxgrantc <pdxgrantc@gmail.com>
2022-12-31 14:51:24 -0800

    remove css

--- shopping-list/src/firebase.js
+++ shopping-list/src/firebase.js
@@ -1,14 +1,12 @@
-import firebase from 'firebase/app';
+import { initializeApp } from "firebase/app";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCD_-9ryz2E4RRouaxeE1bKOl6xoPZVIwY",
-    authDomain: "shopping-list-f4612.firebaseapp.com",
-    projectId: "shopping-list-f4612",
-    storageBucket: "shopping-list-f4612.appspot.com",
-    messagingSenderId: "492725925529",
-    appId: "1:492725925529:web:c0865252c5de8645572a08"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCD_-9ryz2E4RRouaxeE1bKOl6xoPZVIwY",
+  authDomain: "shopping-list-f4612.firebaseapp.com",
+  projectId: "shopping-list-f4612",
+  storageBucket: "shopping-list-f4612.appspot.com",
+  messagingSenderId: "492725925529",
+  appId: "1:492725925529:web:c0865252c5de8645572a08"
+};
+
+const app = initializeApp(firebaseConfig);
