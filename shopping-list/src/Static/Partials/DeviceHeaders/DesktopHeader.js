44334aef0edd0ab3a8787d86d82225c48505808d
pdxgrantc <pdxgrantc@gmail.com>
2023-01-01 11:44:45 -0800

    sign in

--- shopping-list/src/Static/Partials/DeviceHeaders/DesktopHeader.js
+++ shopping-list/src/Static/Partials/DeviceHeaders/DesktopHeader.js
@@ -1,18 +1,9 @@
 import React from 'react'
-
 import { Link } from 'react-router-dom'
+import { auth } from '../../../Firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
 
-import { signInWithGoogle, signOutProvider } from '../../../service/firebase';
-
-import firebase from 'firebase/compat/app'
-import "firebase/compat/firestore";
-import "firebase/compat/auth";
-import { getAuth } from "firebase/auth";
-
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-
-const auth = getAuth();
+import { SignInComponent, SignOutComponent } from '../../../Firebase'
 
 export default function DesktopHeader() {
     const [user] = useAuthState(auth);
@@ -25,11 +16,7 @@ export default function DesktopHeader() {
                     <Link class="h-fit align-middle px-[1.5vw] py-[.5vw] hover:bg-button_pressed_color hover:ease-in-out duration-[350ms]" to="/My-Lists">My Lists</Link>
                     <h2 class="h-fit align-middle px-[1.5vw] py-[.5vw] hover:bg-button_pressed_color hover:ease-in-out duration-[350ms]">Account</h2>
                     <section>
-                        {user ?
-                            <h2 onClick={signOutProvider} class="h-fit align-middle bg-button_accent_color hover:bg-transparent px-[1.5rem] mr-[0rem] py-[.5rem]">Logout</h2>
-                            :
-                            <h2 onClick={signInWithGoogle} class="h-fit align-middle bg-button_accent_color hover:bg-transparent px-[1.5rem] mr-[0rem] py-[.5rem]">Login</h2>
-                        }
+                        {user ? <SignOutComponent /> : <SignInComponent />}
                     </section>
                 </div>
                 {/*<Link class="h-fit align-middle px-[1vw] py-[.5vw] hover:bg-button_pressed_color hover:cursor-pointer hover:ease-in-out duration-[350ms]" to="/Code">Code</Link>*/}
--- shopping-list/src/firebase.js
+++ shopping-list/src/firebase.js
@@ -1,4 +1,8 @@
+import firebase from 'firebase/compat/app'
+
 import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup } from "firebase/auth";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCD_-9ryz2E4RRouaxeE1bKOl6xoPZVIwY",
@@ -10,3 +14,24 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+export function SignInComponent() {
+  const signInWithGoogle = () => {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      signInWithPopup(auth, provider);
+  }
+  return (
+      <>
+          <button class="bg-button_accent_color h-fit align-middle px-[1.5vw] py-[.5vw] hover:bg-button_pressed_color hover:ease-in-out duration-[350ms]" onClick={signInWithGoogle}>Sign In</button>
+      </>
+  )
+}
+
+export function SignOutComponent() {
+  return auth.currentUser && (
+      <>
+          <button class="bg-button_accent_color h-fit align-middle px-[1.5vw] py-[.5vw] hover:bg-button_pressed_color hover:ease-in-out duration-[350ms]" onClick={() => auth.signOut()}>Sign Out</button>
+      </>
+  )
+}
