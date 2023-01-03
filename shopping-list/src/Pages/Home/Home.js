9311cd7aac033d47974f1584404321b8482defa5
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2023-01-03 15:52:25 -0800

    force sign in homepage

--- shopping-list/src/Pages/Home/Home.js
+++ shopping-list/src/Pages/Home/Home.js
@@ -10,6 +10,8 @@ import Footer from '../../Static/Partials/Footer'
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
+import { signInWithPopup } from "firebase/auth";
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 firebase.initializeApp({
     apiKey: "AIzaSyCD_-9ryz2E4RRouaxeE1bKOl6xoPZVIwY",
@@ -24,6 +26,8 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 export default function Home() {
+    const [user] = useAuthState(auth);
+
     return (
         <>
             <Helmet>
@@ -33,14 +37,37 @@ export default function Home() {
                 <Header />
                 <div class="h-[5vh]"></div>
                 <div class="w-full h-[85vh] flex gap-[5vw]">
-                    <div class="w-[26vw] h-full">
-                        <LeftBar />
-                    </div>
-                    <div class="w-full bg-black">
-                        <NewList />
-                    </div>
+                    {user ? <SignedIn /> : <SignedOut />}
                 </div>
                 {/*<Footer />*/}
+            </div>
+        </>
+    )
+}
+
+function SignedIn() {
+    return (
+        <>
+            <div class="w-[26vw] h-full">
+                <LeftBar />
+            </div>
+            <div class="w-full bg-black">
+                <NewList />
+            </div>
+        </>
+    )
+}
+
+function SignedOut() {
+    const signInWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        signInWithPopup(auth, provider);
+    }
+    return (
+        <>
+            <div class="px-[10vw] py-[8vh] w-[90%] mx-auto h-[95%] bg-black">
+                <h2 class="text-[3.25rem]">You must be signed in to Google to use this app</h2>
+                <button onClick={signInWithGoogle}>Sign In</button>
             </div>
         </>
     )
