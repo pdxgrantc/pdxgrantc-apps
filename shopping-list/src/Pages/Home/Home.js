01e0e064e3d59755a537b08806b1193490ccdb80
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2023-01-09 17:39:09 -0800

    Back to working

--- shopping-list/src/Pages/Home/Home.js
+++ shopping-list/src/Pages/Home/Home.js
@@ -78,3 +78,16 @@ function SignedOut() {
         </>
     )
 }
+
+/*
+<div class="px-[5vw] vertical py-[12vh] w-fit mx-auto h-[90%] bg-black">
+                <div>
+                    <h2 class="text-[3.25rem] text-center">You must be signed in</h2>
+                    <h2 class="text-[3.25rem] text-center">to Google to use this app</h2>
+                </div>
+                <div class="h-[9vh]"></div>
+                <div class="m-auto w-fit">
+                    <button class="m-auto cursor-pointer w-fit text-[2.75rem] border-b-[1.5px] on_desktop:hover:bg-button_accent_color on_desktop:hover:ease-[cubic-bezier(0.4, 0, 1, 1)] on_desktop:duration-[350ms] on_desktop:hover:px-[2.75vw] py-[.4vh] w-fit align-middle" onClick={signInWithGoogle}>Sign In With Google</button>
+                </div>
+            </div>
+*/
